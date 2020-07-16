78e5a11a94cf9c361455c9cfaf2fbefa7a26a0c6
Thai Chung <mail@0xreki.de>
2020-07-16 10:00:15 +0200

    Clean Up old files

--- assets/scripts/default.js
+++
@@ -1,6 +0,0 @@
----
----
-{% include_relative jquery/jquery-3.2.1.slim.min.js %}
-{% include_relative bootstrap/bootstrap.min.js %}
-{% include_relative bootstrap/bootstrap-toggle.min.js %}
-{% include_relative base.js %}
